3a5e223f8e34220704beee818397d0f8570d134c
Deastrom <benjamin.cedar@gmail.com>
2021-02-01 13:47:54 -0600

    tons of changes

--- Test-GitOpsDrift.ps1
+++ Test-GitOpsDrift.ps1
@@ -67,30 +67,96 @@ Function Test-GitOpsDrift {
                 $GitStatusArray = $Line.Split("`t")
                 Write-Debug $GitStatusArray.Count
                 If ($GitStatusArray.Count -eq 3) {
-                    $GitSrcFilePath = ([System.IO.FileInfo](Join-Path $SourceDirectory $GitStatusArray[2])).FullName
-                    Write-Debug $GitSrcFilePath
-                    $GitSrcFile = $GitSrcFilePath.Replace("$($SourceDirectory.FullName)", "")
-                    $GitSrcFileFromFile = ([System.IO.FileInfo](Join-Path $SourceDirectory $GitStatusArray[1])).FullName
+                    $GitSrcFileInfo = [System.IO.FileInfo](Join-Path $SourceDirectory $GitStatusArray[2])
+                    Write-Debug $GitSrcFileInfo
+                    $GitSrcFile = $GitSrcFileInfo.FullName.Replace("$($SourceDirectory.FullName)", "")
+                    $GitSrcFromFileInfo = [System.IO.FileInfo](Join-Path $SourceDirectory $GitStatusArray[1])
                     $Files["$GitSrcFile"] = @{
-                        FromFile  = $GitSrcFileFromFile.Replace("$($SourceDirectory.FullName)", "")
+                        FullName = $GitSrcFileInfo.FullName
+                        Parent = $GitSrcFileInfo.Parent
+                        FromFile  = $GitSrcFromFileInfo.FullName.Replace("$($SourceDirectory.FullName)", "")
                         GitStatus = $GitStatusArray[0]
-                        Checked   = $false
                     }
                 }
                 ElseIf ($GitStatusArray.Count -eq 2) {
-                    $GitSrcFilePath = ([System.IO.FileInfo](Join-Path $SourceDirectory $GitStatusArray[1])).FullName
-                    Write-Debug $GitSrcFilePath
-                    $GitSrcFile = $GitSrcFilePath.Replace("$($SourceDirectory.FullName)", "")
+                    $GitSrcFileInfo = [System.IO.FileInfo](Join-Path $SourceDirectory $GitStatusArray[1])
+                    Write-Debug $GitSrcFileInfo
+                    $GitSrcFile = $GitSrcFileInfo.FullName.Replace("$($SourceDirectory.FullName)", "")
                     $Files["$GitSrcFile"] = @{
+                        FullName = $GitSrcFileInfo.FullName
+                        Parent = $GitSrcFileInfo.Parent
                         FromFile  = $GitSrcFile
                         GitStatus = $GitStatusArray[0]
-                        Checked   = $false
                     }
                 }
             }
             Write-Debug "$($Files | ConvertTo-Json -Depth 100)"
+            ForEach ($File in $($Files.GetEnumerator())) {
+                $BuildFile = @{
+                    FullName = Join-Path -Path $BuildDirectory.FullName -ChildPath $File.Key
+                    Directory = Join-Path -Path $BuildDirectory.FullName -ChildPath $File.Value.Parent.Replace("$($SourceDirectory.FullName)", "")
+                }
+                $DestinationFile = @{
+                    FullName = Join-Path -Path $DestinationDirectory.FullName -ChildPath $File.Key
+                    Directory = Join-Path -Path $DestinationDirectory.FullName -ChildPath $File.Value.Parent.Replace("$($SourceDirectory.FullName)", "")
+                }
+                If ($File.Key.EndsWith($TemplateExtension)) {
+                    $BuildFile.FullName = $BuildFile.FullName.Replace($TemplateExtension, "")
+                    $DestinationFile.FullName = $DestinationFile.FullName.Replace($TemplateExtension, "")
+                } ElseIf ($File.Key.Contains($SpectoCommon)) {
+                    If ($SpectoSignature -and ($File.Key.EndsWith($SpectoSignature))) {
+                        $BuildFile.FullName = $BuildFile.FullName.Replace($SpectoCommon, "").Replace($SpectoSignature, "")
+                        $DestinationFile.FullName = $DestinationFile.FullName.Replace($SpectoCommon, "").Replace($SpectoSignature, "")
+                    } Else {
+                        Continue
+                    }
+                }
+                $BuildFileHash = Get-FileHash $BuildFile.FullName
+                $DestinationFileHash = Invoke-Command -ScriptBlock { Get-FileHash $using:DestinationFile.FullName -ErrorAction SilentlyContinue } @InvCmdParams
+                Switch ($File.Value.GitStatus) {
+                    'A' {
+                        # addition of a file
+                        Write-Verbose "$($File.Key) was Added since last build, $($DestinationFile.FullName) hash should be null."
+                        If ($null -ne $DestinationFilehash) {
+                            Write-Warning "$($File.Key) was added but $($DestinationFile.FullName) already exists."
+                        }
+                        Break
+                    }
+                    'D' {
+                        # deletion of a file
+                        Write-Warning "$($File.Key) was Deleted since last build, consider deleting $($DestinationFile.FullName)."
+                        Break
+                    }
+                    'M' {
+                        # modification of the contents or mode of a file
+                        Write-Warning "$($File.Key) was Modified since last build, $($DestinationFile.FullName) shoudl be different than Source and overwritten."
+                        If ($null -ne $DestinationFilehash) {
+                            Write-Warning "$($File.Key) was Modified but $($DestinationFile.FullName) was not found."
+                        } ElseIf ($BuildFileHash.Hash -eq $DestinationFileHash.Hash) {
+                            Write-Warning "$($File.Key) was Modified but $($DestinationFile.FullName) matches the hash of the $($Buildfile.FullName) file. File may have already been deployed."
+                        }
+                        Break
+                    }
+                    'R' {
+                        # renaming of a file
+                        Write-Verbose "$($File.Key) has been renamed from $($File.Value.FromFile). $($DestinationFile.FullName) should not exist. Any file generated from $($Destination.FullName) should be considered for deletion."
+                        If ($null -ne $DestinationFileHash) {
+                            Write-Warning "$($File.Key) was renamed from $($File.Value.FromFile), but $($DestinationFile.FullName) already exists."
+                        }
+                        Write-Warning "$($File.Key) was renamed from $($File.Value.FromFile). Consider deleting any file that was generated from $($File.Value.FromFile)."
+                    }
+                    Default {
+                        # Default
+                        Write-Warning "$($File.Key) has a status of $($File.Value.GitStatus), which is not handled."
+                    }
+                }
+            }
             ForEach ($SourceFile in $(Get-ChildItem $SourceDirectory.FullName -Recurse -File)) {
                 $SourceFileRelPath = $SourceFile.FullName.Replace($SourceDirectory.FullName, "")
+                If ($Files.ContainsKey($SourceFileRelPath)) {
+                    Write-Verbose "$SourceFileRelPath was already checked during Git Diff handling."
+                    Continue
+                }
                 $BuildFile = @{
                     FullName  = Join-Path -Path $BuildDirectory.FullName -ChildPath $SourceFile.FullName.Replace($SourceDirectory.FullName, "")
                     Directory = Join-Path -Path $BuildDirectory.FullName -ChildPath $SourceFile.Directory.FullName.Replace($SourceDirectory.FullName, "")
@@ -113,96 +179,12 @@ Function Test-GitOpsDrift {
                 }
                 $BuildFileHash = Get-FileHash $BuildFile.FullName
                 $DestinationFileHash = Invoke-Command -ScriptBlock { Get-FileHash $using:DestinationFile.FullName -ErrorAction SilentlyContinue } @InvCmdParams
-                $DestinationFileRelPath = $DestinationFile.FullName.Replace($DestinationDirectory.FullName, "")
-                Switch ($Files["$SourceFileRelPath"].GitStatus) {
-                    'A' {
-                        # addition of a file
-                        Write-Verbose "$SourceFileRelPath was Added since last build, $($DestinationDirectory.FullName) Hash should be null."
-                        If ($null -ne $DestinationFileHash) {
-                            Write-Warning "$SourceFileRelPath was Added but $DestinationFileRelPath already exists in $($DestinationDirectory.FullName)"
-                        }
-                        $Files["$SourceFileRelPath"].Checked = $true
-                        Break
-                    }
-                    'C' {
-                        # copy of a file into a new one
-                        Write-Warning "$SourceFileRelPath has as a status of 'C'. This case is not yet handled."
-                        $Files["$SourceFileRelPath"].Checked = $true
-                        Break
-                    }
-                    'D' {
-                        # deletion of a file
-                        Write-Warning "$SourceFileRelPath was Deleted since last build, $($DestinationDirectory.FullName) will be left alone, consider deleting."
-                        $Files["$SourceFileRelPath"].Checked = $true
-                        Break
-                    }
-                    'M' {
-                        # modification of the contents or mode of a file
-                        Write-Verbose "$SourceFileRelPath was Modified since last build, $($DestinationFile.Fullname) should be different than Source and overwritten."
-                        If ($null -ne $DestinationFileHash) {
-                            Write-Warning "$SourceFileRelPath was Modified but $($DestinationFile.FullName) was not found."
-                        }
-                        ElseIf ($BuildFileHash.Hash -eq $DestinationFileHash.Hash) {
-                            Write-Warning "$SourceFileRelPath was Modified but $($DestinationFile.Fullname) matches the hash of the $($BuildFile.FullName) file. File may have already been deployed."
-                        }
-                        $Files["$SourceFileRelPath"].Checked = $true
-                        Break
-                    }
-                    'R' {
-                        # renaming of a file
-                        $fromFile = $Files["$SourceFileRelPath"].FromFile
-                        Write-Verbose "$SourceFileRelPath has been renamed from $fromFile. $($DestinationFile.Fullname) should not exist. Any file generated from $fromFile should be considered for deletion."
-                        If ($null -ne $DestinationFileHash) {
-                            Write-Warning "$SourceFileRelPath was renamed from $fromFile, but $DestinationFileRelPath already exists in $($DestinationDirectory.FullName)"
-                        }
-                        Write-Warning "$SourceFileRelPath was renamed from $fromFile, consider deleting any file that was generated from $fromFile."
-                        $Files["$SourceFileRelPath"].Checked = $true
-                        Break
-                    }
-                    'T' {
-                        # change in the type of the file
-                        Write-Warning "$SourceFileRelPath has as a status of 'T'. This case is not yet handled."
-                        $Files["$SourceFileRelPath"].Checked = $true
-                        Break
-                    }
-                    'U' {
-                        # file is unmerged
-                        Write-Error "$SourceFileRelPath has as a status of 'U'. All files shoudl be merged at this point."
-                        $Files["$SourceFileRelPath"].Checked = $true
-                        Break
-                    }
-                    'X' {
-                        # "unknown" change type (bug)
-                        Write-Error "$SourceFileRelPath has as a status of 'X'. Git 'X' is a unkown change type... there may be a bug."
-                        $Files["$SourceFileRelPath"].Checked = $true
-                        Break
-                    }
-                    Default {
-                        # no git status, no change expected
-                        Write-Verbose "$SourceFileRelPath has no changes since the last build. $($BuildFile.FullName) should match $($DestinationFile.FullName)."
-                        If ($null -eq $DestinationFileHash) {
-                            Write-Warning "$SourceFileRelPath has no changes since the last build. $($DesinationFile.FullName) was not found."
-                        }
-                        ElseIf ($($DestinationFileHash.Hash) -ne $($BuildFileHash.Hash)) {
-                            Write-Warning "$SourceFileRelPath has no changes since the last build. $($BuildFile.FullName) does not match $($DestinationFile.FullName)."
-                        }
-                        Break
-                    }
+                Write-Verbose "$SourceFileRelPath has no changes since the last build. $($BuildFile.FullName) should match $($DestinationFile.FullName)."
+                If ($null -eq $DestinationFileHash) {
+                    Write-Warning "$SourceFileRelPath has no changes since the last build. $($DesinationFile.FullName) was not found."
                 }
-            }
-            ForEach ($File in $($Files.GetEnumerator() | Where-Object checked -eq $false)) {
-                # check all other files, should be the deleted files
-                Switch ($Files["$SourceFileRelPath"].GitStatus) {
-                    'D' {
-                        Write-Warning "$SourceFileRelPath was deleted. Consider deleting $($DestinationFile.FullName)."
-                        $Files["$SourceFileRelPath"].Checked = $true
-                        Break
-                    }
-                    Default {
-                        $gitStatus = $Files["$SourceFileRelPath"].GitStatus
-                        $checked = $Files["$SourceFileRelPath"].Checked
-                        Write-Warning "$SourceFileRelPath has a git status of $gitStatus and has a checked of $checked. This should have already been addressed."
-                    }
+                ElseIf ($($DestinationFileHash.Hash) -ne $($BuildFileHash.Hash)) {
+                    Write-Warning "$SourceFileRelPath has no changes since the last build. $($BuildFile.FullName) does not match $($DestinationFile.FullName)."
                 }
             }
         }
